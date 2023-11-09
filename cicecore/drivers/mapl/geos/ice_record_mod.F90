b469a3ccd15096d0d92929c646a360791bfe67be
bzhao <Bin.Zhao@nasa.gov>
2023-11-09 18:53:24 -0500

    a module for recording and restoring thermo state added

---
+++ cicecore/drivers/mapl/geos/ice_record_mod.F90
@@ -0,0 +1,61 @@
+
+      module ice_record_mod
+
+      use ice_kinds_mod
+      use ice_constants, only: field_loc_center, field_type_scalar, c0
+      use ice_domain_size, only: max_blocks, ncat
+      use ice_blocks, only: nx_block, ny_block
+      use ice_state , only: trcrn
+      use ice_exit, only: abort_ice
+      use ice_fileunits, only: nu_diag
+      use icepack_intfc, only: icepack_query_tracer_sizes
+      use icepack_intfc, only: icepack_warnings_flush, icepack_warnings_aborted
+
+      implicit none
+      private
+      public :: alloc_record_state, save_record_state, restore_record_state
+
+
+      real (kind=dbl_kind), &
+         dimension (:,:,:,:,:), allocatable :: &
+         trcrn_save     ! tracers
+                   ! 1: surface temperature of ice/snow (C)
+
+      contains 
+
+      subroutine alloc_record_state
+      integer (int_kind) :: ntrcr, ierr
+      character(len=*),parameter :: subname='(alloc_record_state)'
+
+      call icepack_query_tracer_sizes(ntrcr_out=ntrcr)
+      call icepack_warnings_flush(nu_diag)
+      if (icepack_warnings_aborted()) call abort_ice(error_message=subname, &
+          file=__FILE__, line=__LINE__)
+
+      allocate ( &
+         trcrn_save   (nx_block,ny_block,ntrcr,ncat,max_blocks) , & ! tracers: 1: surface temperature of ice/snow (C)
+         stat=ierr)
+      if (ierr/=0) call abort_ice('(alloc_record_state): Out of memory1')
+
+
+      trcrn_save = c0
+
+      end subroutine alloc_record_state
+
+      subroutine save_record_state
+
+      character(len=*),parameter :: subname='(save_record_state)'
+
+      trcrn_save(:,:,:,:,:) = trcrn(:,:,:,:,:)
+
+      end subroutine save_record_state
+
+      subroutine restore_record_state
+
+      character(len=*),parameter :: subname='(restore_record_state)'
+
+      trcrn(:,:,:,:,:) = trcrn_save(:,:,:,:,:)
+
+      end subroutine restore_record_state
+
+      end module ice_record_mod
