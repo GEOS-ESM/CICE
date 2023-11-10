05d1ed5ee74bf6600a01954fb086bfcf99f729d8
bzhao <Bin.Zhao@nasa.gov>
2023-11-10 15:58:13 -0500

    zero out many fluxes which could have accumulated during predictor step

--- cicecore/drivers/mapl/geos/ice_record_mod.F90
+++ cicecore/drivers/mapl/geos/ice_record_mod.F90
@@ -7,9 +7,11 @@
       use ice_communicate, only: my_task, master_task
       use ice_blocks,      only: nx_block, ny_block
       use ice_state,       only: aicen, vicen, vsnon, trcrn
+      use ice_flux
       use ice_exit,        only: abort_ice
       use ice_fileunits,   only: nu_diag
       use icepack_intfc,   only: icepack_query_tracer_sizes
+      use icepack_intfc,   only: icepack_query_parameters
       use icepack_intfc,   only: icepack_warnings_flush, icepack_warnings_aborted
 
       implicit none
@@ -47,6 +49,9 @@
         if (ierr/=0) call abort_ice('(alloc_record_state): Out of memory1')
 
 
+        aicen_save = c0
+        vicen_save = c0
+        vsnon_save = c0
         trcrn_save = c0
 
       end subroutine alloc_record_state
@@ -67,13 +72,27 @@
 
          if(my_task == master_task) then
             write(*,*), 'CICE6 thermo state saved'
-         endif 
+         endif
 
       end subroutine save_record_state
 
       subroutine restore_record_state
 
+         use ice_flux_bgc, only: flux_bio_atm, flux_bio, faero_atm, fiso_atm, &
+           fnit, famm, fsil, fdmsp, fdms, fhum, fdust, falgalN, &
+           fdoc, fdon, fdic, ffed, ffep
+
+
          character(len=*),parameter :: subname='(restore_record_state)'
+         real (kind=dbl_kind)       :: stefan_boltzmann, Tffresh
+
+         call icepack_query_parameters(stefan_boltzmann_out=stefan_boltzmann, &
+                       Tffresh_out=Tffresh)
+         call icepack_warnings_flush(nu_diag)
+         if (icepack_warnings_aborted()) call abort_ice(error_message=subname, &
+              file=__FILE__, line=__LINE__)
+
+
          if (.not. allocated(trcrn_save)) &
             call abort_ice(error_message=subname//': trcrn_save not allocated', &
             file=__FILE__, line=__LINE__)
@@ -83,9 +102,63 @@
          vsnon(:,:,:,:) = vsnon_save(:,:,:,:)
          trcrn(:,:,:,:,:) = trcrn_save(:,:,:,:,:)
 
+
+         fsens   (:,:,:) = c0
+         flat    (:,:,:) = c0
+         fswabs  (:,:,:) = c0
+         fswint_ai(:,:,:) = c0
+         flwout  (:,:,:) = -stefan_boltzmann*Tffresh**4
+                       ! in case atm model diagnoses Tsfc from flwout
+         evap    (:,:,:) = c0
+         evaps   (:,:,:) = c0
+         evapi   (:,:,:) = c0
+         Tref    (:,:,:) = c0
+         Qref    (:,:,:) = c0
+         Uref    (:,:,:) = c0
+
+
+
+         fresh   (:,:,:) = c0
+         fsalt   (:,:,:) = c0
+         fpond   (:,:,:) = c0
+         fhocn   (:,:,:) = c0
+         fswthru (:,:,:) = c0
+         fswthru_vdr (:,:,:) = c0
+         fswthru_vdf (:,:,:) = c0
+         fswthru_idr (:,:,:) = c0
+         fswthru_idf (:,:,:) = c0
+         fswthru_uvrdr (:,:,:) = c0
+         fswthru_uvrdf (:,:,:) = c0
+         fswthru_pardr (:,:,:) = c0
+         fswthru_pardf (:,:,:) = c0
+
+         alvdr   (:,:,:) = c0
+         alidr   (:,:,:) = c0
+         alvdf   (:,:,:) = c0
+         alidf   (:,:,:) = c0
+
+
+         fresh_da(:,:,:) = c0    ! data assimilation
+         fsalt_da(:,:,:) = c0
+         flux_bio(:,:,:,:) = c0 ! bgc
+         fnit    (:,:,:) = c0
+         fsil    (:,:,:) = c0
+         famm    (:,:,:) = c0
+         fdmsp   (:,:,:) = c0
+         fdms    (:,:,:) = c0
+         fhum    (:,:,:) = c0
+         fdust   (:,:,:) = c0
+         falgalN(:,:,:,:)= c0
+         fdoc   (:,:,:,:)= c0
+         fdic   (:,:,:,:)= c0
+         fdon   (:,:,:,:)= c0
+         ffep   (:,:,:,:)= c0
+         ffed   (:,:,:,:)= c0
+
+
          if(my_task == master_task) then
             write(*,*), 'CICE6 thermo state restored'
-         endif 
+         endif
 
       end subroutine restore_record_state
 
