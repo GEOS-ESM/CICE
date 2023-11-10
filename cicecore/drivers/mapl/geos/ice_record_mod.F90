42e0c12bb6962d9ade83b1be416600b6a7d50403
bzhao <Bin.Zhao@nasa.gov>
2023-11-10 13:08:55 -0500

    need to save addtional states to restore later; turn on print out

--- cicecore/drivers/mapl/geos/ice_record_mod.F90
+++ cicecore/drivers/mapl/geos/ice_record_mod.F90
@@ -6,7 +6,7 @@
       use ice_domain_size, only: max_blocks, ncat
       use ice_communicate, only: my_task, master_task
       use ice_blocks,      only: nx_block, ny_block
-      use ice_state,       only: trcrn
+      use ice_state,       only: aicen, vicen, vsnon, trcrn
       use ice_exit,        only: abort_ice
       use ice_fileunits,   only: nu_diag
       use icepack_intfc,   only: icepack_query_tracer_sizes
@@ -15,6 +15,12 @@
       implicit none
       private
       public :: alloc_record_state, save_record_state, restore_record_state
+
+      real (kind=dbl_kind), &
+         dimension (:,:,:,:), allocatable :: &
+         aicen_save , & ! concentration of ice
+         vicen_save , & ! volume per unit area of ice          (m)
+         vsnon_save     ! volume per unit area of snow         (m)
 
 
       real (kind=dbl_kind), &
@@ -33,7 +39,10 @@
         if (icepack_warnings_aborted()) call abort_ice(error_message=subname, &
           file=__FILE__, line=__LINE__)
 
-        allocate (trcrn_save (nx_block,ny_block,ntrcr,ncat,max_blocks) , & ! tracers: 1: surface temperature of ice/snow (C)
+        allocate ( aicen_save(nx_block,ny_block,ncat,max_blocks) , & ! concentration of ice
+                   vicen_save(nx_block,ny_block,ncat,max_blocks) , & ! volume per unit area of ice (m)
+                   vsnon_save(nx_block,ny_block,ncat,max_blocks) , & ! volume per unit area of snow (m)
+                   trcrn_save(nx_block,ny_block,ntrcr,ncat,max_blocks) , & ! tracers: 1: surface temperature of ice/snow (C)
                 stat=ierr)
         if (ierr/=0) call abort_ice('(alloc_record_state): Out of memory1')
 
@@ -50,11 +59,15 @@
             call abort_ice(error_message=subname//': trcrn_save not allocated', &
             file=__FILE__, line=__LINE__)
 
+            
+         aicen_save(:,:,:,:) = aicen(:,:,:,:)
+         vicen_save(:,:,:,:) = vicen(:,:,:,:)
+         vsnon_save(:,:,:,:) = vsnon(:,:,:,:)
          trcrn_save(:,:,:,:,:) = trcrn(:,:,:,:,:)
 
-         !if(my_task == master_task) then
-         !   write(*,*), 'thermo state saved'
-         !endif 
+         if(my_task == master_task) then
+            write(*,*), 'CICE6 thermo state saved'
+         endif 
 
       end subroutine save_record_state
 
@@ -65,7 +78,14 @@
             call abort_ice(error_message=subname//': trcrn_save not allocated', &
             file=__FILE__, line=__LINE__)
 
+         aicen(:,:,:,:) = aicen_save(:,:,:,:)
+         vicen(:,:,:,:) = vicen_save(:,:,:,:)
+         vsnon(:,:,:,:) = vsnon_save(:,:,:,:)
          trcrn(:,:,:,:,:) = trcrn_save(:,:,:,:,:)
+
+         if(my_task == master_task) then
+            write(*,*), 'CICE6 thermo state restored'
+         endif 
 
       end subroutine restore_record_state
 
